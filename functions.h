15eed21178ed960dbfabab764948c5318d568f47
Daniel Carneros Mateu <theDn2000@gmail.com>
2024-06-17 14:06:03 +0200

    Update functions.h

--- functions.h
+++ functions.h
@@ -25,3 +25,5 @@ struct Ball
 Player parseInitialMessage(std::string &message, Player &player);
 
 vector<string> separate_string(string & s);
+
+vector<string> separate_string_separator(string & s, string separator);
