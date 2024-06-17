e802141356f90a572b25e5282f81b3f94f91b005
Daniel Carneros Mateu <theDn2000@gmail.com>
2024-06-17 11:46:50 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,49 +13,6 @@ using namespace std;
 
 #include "functions.h"
 
-// Define Player struct
-
-struct Player
-{
-    std::string team_name;
-    std::string playmode;
-    std::string side;
-    int unum;
-    double x;
-    double y;
-};
-
-
-// Parse initial message function (init Side Unum PlayMode)
-Player parseInitialMessage(std::string &message, Player &player)
-{
-    std::string delimiter = " ";
-    size_t pos = 0;
-    std::string token;
-    int i = 0;
-    while ((pos = message.find(delimiter)) != std::string::npos)
-    {
-        token = message.substr(0, pos);
-        if (i == 1)
-        {
-            player.side = token;
-        }
-        else if (i == 2)
-        {
-            player.unum = std::stoi(token);
-        }
-        else if (i == 3)
-        {
-            player.playmode = token;
-        }
-        message.erase(0, pos + delimiter.length());
-        i++;
-    }
-    cout << "Player: " << player.side << " " << player.unum << " " << player.playmode << endl;
-    return player;
-}
-
-
 // Move command function
 void sendInitialMoveMessage(const Player &player, MinimalSocket::udp::Udp<true> &udp_socket, MinimalSocket::Address const &recipient)
 {
