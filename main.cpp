edee3f7aacd9c885ee091cafe4cf98e36cf88485
Daniel Carneros Mateu <theDn2000@gmail.com>
2024-06-17 11:43:34 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -140,8 +140,8 @@ int main(int argc, char *argv[])
         auto received_message = udp_socket.receive(message_max_size);
         std::string received_message_content = received_message->received_message;
 
-        std::string parsed_message = separate_string(received_message_content);
-        cout << "Received message: " << parsed_message << endl;
+        vector<string> parsed_message = separate_string(received_message_content);
+        cout << "Received message: " << parsed_message[0] << endl;
 
 
 
