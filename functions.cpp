ab0c76a0b974bbe5b12322f6b265ea964cfd958a
Daniel Carneros Mateu <theDn2000@gmail.com>
2024-06-17 12:01:05 +0200

    a

--- functions.cpp
+++ functions.cpp
@@ -7,15 +7,7 @@ using namespace std;
 // Include the header file
 #include "functions.h"
 
-struct Player
-{
-    std::string team_name;
-    std::string playmode;
-    std::string side;
-    int unum;
-    double x;
-    double y;
-};
+
 // Functions
 
 // Parse initial message function (init Side Unum PlayMode)
