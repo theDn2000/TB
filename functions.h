25bafc42c2911a37c6e8139976d92993782222bb
Daniel Carneros Mateu <theDn2000@gmail.com>
2024-06-20 11:10:00 +0200

    Update functions.h

--- functions.h
+++ functions.h
@@ -5,25 +5,6 @@
 #include <cmath>
 using namespace std;
 
-struct Player
-{
-    std::string team_name;
-    std::string playmode;
-    std::string side;
-    bool see_ball;
-    int unum;
-    double x;
-    double y;
-};
-
-struct Ball
-{
-    std::string x;
-    std::string y;
-    std::string vx;
-    std::string vy;
-};
-
 Player parseInitialMessage(std::string &message, Player &player);
 
 vector<string> separate_string(string & s);
