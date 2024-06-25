48e42c00ce491362adddea4ae1de4b46ba52efdd
Daniel Carneros Mateu <theDn2000@gmail.com>
2024-06-25 12:22:29 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -104,7 +104,7 @@ int main(int argc, char *argv[])
 
     while (true)
     {
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
         auto received_message = udp_socket.receive(message_max_size);
         std::string received_message_content = received_message->received_message;
 
