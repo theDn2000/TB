821e1a7f7250d2485ac91917f0de1ac31e0ade57
Daniel Carneros Mateu <theDn2000@gmail.com>
2024-06-27 23:07:31 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -135,7 +135,7 @@ int main(int argc, char *argv[])
 
 
             // Trilateration
-            vector<vector<double>> flags = {field.flag_center, field.flag_center_top, field.flag_center_bottom, field.flag_left_top, field.flag_left_bottom, field.flag_right_top, field.flag_right_bottom, field.flag_penalty_left_top, field.flag_penalty_left_center, field.flag_penalty_left_bottom, field.flag_penalty_right_top, field.flag_penalty_right_center, field.flag_penalty_right_bottom, field.flag_goal_left_top, field.flag_goal_left_bottom};
+            vector<vector<double>> flags = {field.flag_center, field.flag_center_top, field.flag_center_bottom, field.flag_left_top, field.flag_left_bottom, field.flag_right_top, field.flag_right_bottom, field.flag_penalty_left_top, field.flag_penalty_left_center, field.flag_penalty_left_bottom, field.flag_penalty_right_top, field.flag_penalty_right_center, field.flag_penalty_right_bottom, field.flag_goal_left_top, field.flag_goal_left_bottom, field.flag_goal_right_top, field.flag_goal_right_bottom};
             vector<vector<double>> flags_abs = {field.flag_center_abs, field.flag_center_top_abs, field.flag_center_bottom_abs, field.flag_left_top_abs, field.flag_left_bottom_abs, field.flag_right_top_abs, field.flag_right_bottom_abs, field.flag_penalty_left_top_abs, field.flag_penalty_left_center_abs, field.flag_penalty_left_bottom_abs, field.flag_penalty_right_top_abs, field.flag_penalty_right_center_abs, field.flag_penalty_right_bottom_abs, field.flag_goal_left_top_abs, field.flag_goal_left_bottom_abs};
             
             if (player.flags_seen >= 3) // Trilateration can be calculated with 3 flags
@@ -176,7 +176,6 @@ int main(int argc, char *argv[])
                             cout << "P3: " << P3[0] << " " << P3[1] << endl;
                             cout << "D3: " << D3 << endl;
                         }
-                        cout << "Flags used:" <<flags_used << endl;
                     }
                 }
                 if (flags_used == 3)
