1d34be699a3b1240395ac8743911e72252465b3b
Daniel Carneros Mateu <theDn2000@gmail.com>
2024-06-27 19:32:24 +0200

    Update functions_test.cpp

--- tests/functions_test.cpp
+++ tests/functions_test.cpp
@@ -8,11 +8,11 @@ using namespace std;
 TEST(TrilaterationTest, TrilaterationTest)
 {
     vector<float> P1 = {0, 0};
-    vector<float> P2 = {-1, 0};
-    vector<float> P3 = {0, -1};
-    float D1 = 1;
-    float D2 = 1;
-    float D3 = 1;
+    vector<float> P2 = {0, -34};
+    vector<float> P3 = {0, 33.5};
+    float D1 = 42.6;
+    float D2 = 71.3;
+    float D3 = 64.33;
     vector<float> result = trilateration(P1, P2, P3, D1, D2, D3);
     vector<float> expected = {-0.5, -0.5};
     for (int i = 0; i < 2; i++)
