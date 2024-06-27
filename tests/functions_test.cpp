1c989ca6efb5a3e56c72ac6d909f50977fa1339e
Daniel Carneros Mateu <theDn2000@gmail.com>
2024-06-27 19:07:36 +0200

    Update functions_test.cpp

--- tests/functions_test.cpp
+++ tests/functions_test.cpp
@@ -10,7 +10,10 @@ TEST(TrilaterationTest, TrilaterationTest)
     vector<float> P1 = {0, 0};
     vector<float> P2 = {1, 0};
     vector<float> P3 = {0, 1};
-    vector<float> result = trilateration(P1, P2, P3);
+    float D1 = 1;
+    float D2 = 1;
+    float D3 = 1;
+    vector<float> result = trilateration(P1, P2, P3, D1, D2, D3);
     vector<float> expected = {0.5, 0.5};
     for (int i = 0; i < 2; i++)
     {
