4603e737a01288429d3e268466fca72bcfd7ea35
Daniel Carneros Mateu <theDn2000@gmail.com>
2024-06-30 01:45:51 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -239,14 +239,14 @@ int main(int argc, char *argv[])
     const std::vector<Point2D> flags = 
     {
         {0.0, 0.0},        // Center of the field
-        {0.0, -33.5}       // Top center
+        {0.0, -33.5},       // Top center
         {0.0, 33.5},       // Bottom center
         {-52.5, -33.5},    // Corner top-left
         {-52.5, 33.5},     // Corner bottom-left
         {52.5, -33.5},     // Corner top-right
         {52.5, 33.5},      // Corner bottom-right
         {-36, -20},        // Penalty top-left
-        {-36, 0}           // Penalty center-left
+        {-36, 0},           // Penalty center-left
         {-36, 20},         // Penalty bottom-left
         {36, -20},         // Penalty top-right
         {36, 0},           // Penalty center-right
