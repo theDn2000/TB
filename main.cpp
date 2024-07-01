d93e325ee93906b8fd4af921e8f0a7b7b12d0fef
Daniel Carneros Mateu <theDn2000@gmail.com>
2024-07-01 13:49:01 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -565,6 +565,7 @@ int main(int argc, char *argv[])
             {
                 // The player is not in his zone, run back to the zone
                 //std::string dash_command = moveToZone(player.orientation, {player.x, player.y}, player.zone);
+                int power = 100;
                 std::string dash_command = "(dash " + to_string(power) + " 0)";
                 udp_socket.sendTo(dash_command, server_udp);
             }
