fcf62f051b98593ede1d4af8aae567884b07f5da
Daniel Carneros Mateu <theDn2000@gmail.com>
2024-07-02 12:17:04 +0200

    Orientation print

--- main.cpp
+++ main.cpp
@@ -568,6 +568,7 @@ int main(int argc, char *argv[])
                 std::string dash_command = ("dash 100 180");
                 udp_socket.sendTo(dash_command, server_udp);
             }
+            cout << "Orientation: " << player.orientation << endl;
         }
         std::this_thread::sleep_for(std::chrono::milliseconds(10));
     }
