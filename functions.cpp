a52e2242da80af8d5a7995574fafbfb98e98a97b
Daniel Carneros Mateu <theDn2000@gmail.com>
2024-07-03 13:54:10 +0200

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -54,6 +54,7 @@ void sendInitialMoveMessage(Player &player, MinimalSocket::udp::Udp<true> &udp_s
                       {-5, 0},
                       {-15, 0},
                       {-8, -20}};
+                      
     Posicion myPos = posiciones[player.unum - 1];
     auto moveCommand = "(move " + to_string(myPos.x) + " " + to_string(myPos.y) + ")";
     udp_socket.sendTo(moveCommand, recipient);
